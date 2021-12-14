e70eef9ceb604a8373a23e819be6f064658a2c5f
Evans <evanske2@gmail.com>
2021-12-13 20:09:09 -0500

    changing the menu icon on click

---
+++ new-feature.js
@@ -0,0 +1,19 @@
+// make the hamburger menu BETTER with JS
+     // on click slide in the menu
+          // change the burger icon to a close icon
+          // <i class="fas fa-times"></i>
+     // on click outside the menu, close the menu
+
+const burgerMenu = document.querySelector('.fa-bars');
+     
+console.log(burgerMenu)
+
+burgerMenu.addEventListener('click', function() {
+     // console.log('did it work');
+     toggleMenuIcon(event.target);
+});
+
+function toggleMenuIcon(menuIcon) {
+     menuIcon.classList.toggle('fa-bars');
+     menuIcon.classList.toggle('fa-times');
+};
